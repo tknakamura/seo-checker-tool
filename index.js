29324e4f4951f600051f432845407116f924550a
tknakamura <nakamura@likepass.net>
2025-09-17 08:04:29 +0900

    Force deploy: Add comment to trigger Render.com rebuild

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); // Force deploy trigger
 const axios = require('axios');
 const cheerio = require('cheerio');
 const lighthouse = require('lighthouse');
